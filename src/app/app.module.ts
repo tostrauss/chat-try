8fc83b1f7c25f708469625644d0b7e2a54a6b997
tostrauss <tobias.p.strauss@gmail.com>
2024-06-17 00:33:06 +0200

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';  
+import { RouterModule } from '@angular/router';
+// @ts-ignore
+import { HttpClientModule } from '@angular/common/http';  
 import { AppComponent } from './app.component';
 import { AppRoutingModule, routes } from './app.routing.module';
 import { ChatHomeComponent } from './chathome/chathome.component';
 import { ChatRoomComponent } from './chatroom/chatroom.component';
 import { EmojiModule } from '@ctrl/ngx-emoji-mart/ngx-emoji';
 import { PickerComponent } from '@ctrl/ngx-emoji-mart';
-import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
